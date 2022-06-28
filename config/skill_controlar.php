f7418dd83223225c584dd662b15f1bb01eae8814
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-29 00:34:57 +0600

    All Add Update Section Added in 29-06-2022

--- config/skill_controlar.php
+++ config/skill_controlar.php
@@ -2,24 +2,25 @@
 
 include "./server_connect.php";
 
-if(isset($_POST["skillSubmit"])){
+if (isset($_POST["skillSubmit"])) {
     $SkillName = $_POST['SkillName'];
     $Percentage = $_POST['Percentage'];
     $Experience = $_POST['Experience'];
-    
-    
-    if(empty($SkillName) || empty($Percentage) || empty($Experience)){
+
+
+
+
+    if (empty($SkillName) || empty($Percentage) || empty($Experience)) {
         $message = "All File Required";
-    }else{
+    } else {
         $insert_skill = "INSERT INTO `skill`(`skill_name`, `percentage`, `experience`) VALUES ('$SkillName','$Percentage','$Experience')";
-        
+
         $insert_skill_list = mysqli_query($db_config, $insert_skill);
-    
-        if($insert_skill_list == true){
+
+        if ($insert_skill_list == true) {
             $message = "Insert Success";
-        }else{
+        } else {
             $message = "Insert Fail";
-            
         }
     }
     header("location: ../skill.php?msg={$message}");
