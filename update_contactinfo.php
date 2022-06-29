4f529b8aae70d6efc33657ac41d80bfc66f66697
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-29 22:39:07 +0600

    Contact  Section Update info done - 29/06/2022

--- update_contactinfo.php
+++ update_contactinfo.php
@@ -41,8 +41,8 @@
                 <!-- Page header -->
                 <div class="breadcrumb-line">
                     <ul class="breadcrumb">
-                        <li><a href="index.html"><i class="icon-home2 position-left"></i> Deshbord</a></li>
-                        <li><a href="components_notifications_other.html">Home</a></li>
+                        <li><a href="index.php"><i class="icon-home2 position-left"></i> Deshbord</a></li>
+                        <li><a href="contact.php">Contact</a></li>
                     </ul>
                 </div>
                 <!-- /page header -->
@@ -54,7 +54,6 @@
                     <!-- Horizontal form modal -->
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <h5 class="modal-title">Update Your Contact Page</h5>
                         </div>
 
@@ -64,8 +63,27 @@
                         $getResult = mysqli_query($db_config, $getSelectQry);
 
                         ?>
+                        <!-- =====Message Show======= -->
 
-                        <form action="#" class="form-horizontal">
+                        <?php
+                        if (isset($_GET['msg'])) {
+
+                        ?>
+
+                            <div class="alert bg-success alert-styled-left">
+                                <button type="button" class="close" data-dismiss="alert"><span>&times;</span><span class="sr-only">Close</span></button>
+                                <span class="text-semibold">Good Job</span>
+                                <?php echo $_GET['msg'] ?>
+                            </div>
+
+                        <?php } ?>
+
+                        <!-- =====Message Show======= -->
+
+
+
+
+                        <form class="form-horizontal" action="./config/contact_controlar.php" method="POST">
                             <div class="modal-body">
 
                                 <?php
@@ -75,25 +93,26 @@
 
 
                                 ?>
+                                    <input type="hidden" name="cont_id" value="<?php echo $singleData['id'] ?>">
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="Title">Title</label>
                                         <div class="col-sm-9">
-                                            <input type="text" placeholder="Type Title" class="form-control" id="Title" value="<?php echo $singleData['title'] ?>">
+                                            <input type="text" placeholder="Type Title" class="form-control" id="Title" value="<?php echo $singleData['title'] ?>" name="Title" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="ShortBio">Short Bio</label>
                                         <div class="col-sm-9">
-                                            <input type="text" placeholder="Type your site short discription" id="ShortBio" class="form-control" value="<?php echo $singleData['short_bio'] ?>">
+                                            <input type="text" placeholder="Type your site short discription" id="ShortBio" class="form-control" value="<?php echo $singleData['short_bio'] ?>" name="ShortBio" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="Email">E-mail</label>
                                         <div class="col-sm-9">
-                                            <input type="text" placeholder="Type your email" class="form-control" id="Email" value="<?php echo $singleData['email'] ?>">
+                                            <input type="email" placeholder="Type your email" class="form-control" id="Email" value="<?php echo $singleData['email'] ?>" name="Email" required>
                                         </div>
                                     </div>
 
@@ -101,7 +120,7 @@
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="PhoneNumber">Phone Number</label>
                                         <div class="col-sm-9">
-                                            <input type="text" placeholder="Type your phone number" class="form-control" id="PhoneNumber" value="<?php echo $singleData['phone_number'] ?>">
+                                            <input type="text" placeholder="Type your phone number" class="form-control" id="PhoneNumber" value="<?php echo $singleData['phone_number'] ?>" name="PhoneNumber" required>
                                         </div>
                                     </div>
 
@@ -110,7 +129,7 @@
 
                             <div class="modal-footer">
                                 <a href="contact.php"><button type="button" class="btn btn-link" data-dismiss="modal">Close</button></a>
-                                <button type="submit" class="btn btn-primary">Update</button>
+                                <button type="submit" class="btn btn-primary" name="updateCont">Update Contact Info</button>
                             </div>
                         </form>
                     </div>
