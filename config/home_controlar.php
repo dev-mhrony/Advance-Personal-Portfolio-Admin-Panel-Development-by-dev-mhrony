75788194db649f57063f010d3f301f6ad562610b
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-29 15:11:08 +0600

    Home Section Update Done - 29-06-2022

--- config/home_controlar.php
+++ config/home_controlar.php
@@ -1,14 +1,44 @@
 <?php
 
+// require "server_connect.php";
+
+// if (isset($_POST["updateHome"])) {
+
+//     $home_id = $_POST['home_id'];
+//     $YourName = $_POST['yourName'];
+//     $Description = $_POST['description'];
+//     $Link = $_POST['link'];
+//     $Image = $_POST['image'];
+
+//     if (empty($YourName) || empty($Description) || empty($Link)) {
+//         $message = "All File Required";
+//     } else {
+
+//         $updateQry = "UPDATE home_edit_section SET your_name='{$YourName}', description='{$Description}', link='{$Link}', image='{$Image}' WHERE id='{$home_id}'";
+
+//         $issubmit = mysqli_query($db_config, $updateQry);
+
+
+//         if ($issubmit == true) {
+//             $message = "Update Success";
+//         } else {
+//             $message = "Update Fail";
+//         }
+//     }
+//     header("location: ../update_home_section.php?home_id={$home_id}&msg={$message}");
+// } 
+
+
+
 require "server_connect.php";
 
 if (isset($_POST["updateHome"])) {
 
     $home_id = $_POST['home_id'];
-    $YourName = $_POST['yourName'];
-    $Description = $_POST['description'];
-    $Link = $_POST['link'];
-    $Image = $_POST['image'];
+    $YourName = $_POST['YourName'];
+    $Description = $_POST['Description'];
+    $Link = $_POST['Link'];
+    $Image = $_POST['Image'];
 
     if (empty($YourName) || empty($Description) || empty($Link)) {
         $message = "All File Required";
