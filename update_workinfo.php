ab4f493d44e2ae98e639bd2d2a1119f261c5e4f2
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-29 16:45:45 +0600

    About & Work info Update Done - 29-06-2022

--- update_workinfo.php
+++ update_workinfo.php
@@ -52,21 +52,37 @@
 
                     <?php
                     $work_id = $_GET['work_id'];
-                    $getSingleDataQry = "SELECT * FROM about_work WHERE id=$work_id";
+                    $getSingleDataQry = "SELECT * FROM about_work WHERE id='{$work_id}'";
                     $result = mysqli_query($db_config, $getSingleDataQry);
 
 
 
                     ?>
 
+
+                    <!-- Retun Message to Submite Info Start -->
+                    <?php
+                    if (isset($_GET['msg'])) {
+
+                    ?>
+
+                        <div class="alert bg-success alert-styled-left">
+                            <button type="button" class="close" data-dismiss="alert"><span>&times;</span><span class="sr-only">Close</span></button>
+                            <span class="text-semibold">Good Job</span>
+                            <?php echo $_GET['msg'] ?>
+                        </div>
+
+                    <?php } ?>
+
+                    <!-- Retun Message to Submite Info End -->
+
                     <!-- Horizontal form modal -->
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h5 class="modal-title">Update Work Info</h5>
+                            <h5 class="modal-title">Update <code>Work</code> Info</h5>
                         </div>
 
-                        <form action="#" class="form-horizontal">
+                        <form action="./config/work_controlar.php" class="form-horizontal" method="POST">
                             <div class="modal-body">
 
                                 <?php
@@ -74,32 +90,38 @@
                                 foreach ($result as $key => $singleData) {
 
                                 ?>
+                                    <!-- ======== Hidden Input File to Send Controlar Page Start=========== -->
+
+                                    <input type="hidden" name="work_id" value="<?php echo $singleData['id'] ?>">
+
+                                    <!-- ========== Hidden Input File to Send Controlar Page End============ -->
+
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="Experience">Experience (Year)</label>
                                         <div class="col-sm-9">
-                                            <input type="number" placeholder="Type your fast name" id="Experience" class="form-control" value="<?php echo $singleData['experience_by_year'] ?>">
+                                            <input type="number" placeholder="Type your fast name" id="Experience" class="form-control" value="<?php echo $singleData['experience_by_year'] ?>" name="Experience">
                                         </div>
                                     </div>
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="HappyCustomar">Happy Customar</label>
                                         <div class="col-sm-9">
-                                            <input type="number" placeholder="Type your project lirst name" id="HappyCustomar" class="form-control" value="<?php echo $singleData['happy_customar'] ?>">
+                                            <input type="number" placeholder="Type your project lirst name" id="HappyCustomar" class="form-control" value="<?php echo $singleData['happy_customar'] ?>" name="HappyCustomar">
                                         </div>
                                     </div>
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="CompletProject">Complet Project</label>
                                         <div class="col-sm-9">
-                                            <input type="number" placeholder="Type your date of birth year" id="CompletProject" class="form-control" value="<?php echo $singleData['complet_project'] ?>">
+                                            <input type="number" placeholder="Type your date of birth year" id="CompletProject" class="form-control" value="<?php echo $singleData['complet_project'] ?>" name="CompletProject">
                                         </div>
                                     </div>
 
                                     <div class="form-group">
                                         <label class="control-label col-sm-3" for="Awards">Awards</label>
                                         <div class="col-sm-9">
-                                            <input type="number" placeholder="Type your nationality" class="form-control" id="Awards" value="<?php echo $singleData['awards'] ?>">
+                                            <input type="number" placeholder="Type your nationality" class="form-control" id="Awards" value="<?php echo $singleData['awards'] ?>" name="Awards">
                                         </div>
                                     </div>
 
@@ -115,7 +137,7 @@
 
                             <div class="modal-footer">
                                 <a href="work_info.php"><button type="button" class="btn btn-link" data-dismiss="modal">Close</button></a>
-                                <button type="submit" class="btn btn-primary">Update Work</button>
+                                <button type="submit" class="btn btn-primary" name="UpdateWork">Update Work</button>
                             </div>
 
                         </form>
