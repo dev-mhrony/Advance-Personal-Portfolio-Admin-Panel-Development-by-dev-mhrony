f96419c8cde590782f84b51cd89db413d08c6b6d
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-07-01 16:39:12 +0600

    Image Upload an Preview Done - 01/07/2022

--- DataBase/personal_portfolio.sql
+++ DataBase/personal_portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2022 at 10:15 AM
+-- Generation Time: Jul 01, 2022 at 12:38 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.30
 
@@ -46,7 +46,7 @@ CREATE TABLE `about_info` (
 --
 
 INSERT INTO `about_info` (`id`, `fast_name`, `last_name`, `age`, `nationality`, `freelance`, `address`, `phone_number`, `email`, `language`, `status`) VALUES
-(1, 'MD', 'RONY', 21, 'Bangladesh', '', 'Sonatola, Bogura.', 1608445456, 'abc@gmail.com', 'Bangla', 1);
+(1, 'MD', 'RONY', 21, 'Bangladesh', '123', 'Sonatola, Bogura.', 1608445456, 'abc@gmail.com', 'Bangla', 1);
 
 -- --------------------------------------------------------
 
@@ -68,7 +68,33 @@ CREATE TABLE `about_work` (
 --
 
 INSERT INTO `about_work` (`id`, `experience_by_year`, `happy_customar`, `complet_project`, `awards`, `cv`) VALUES
-(1, 3, 59, 78, 24, 'abc.pdf');
+(1, 3, 555, 453, 24, 'abc.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_seting`
+--
+
+CREATE TABLE `account_seting` (
+  `id` int(11) NOT NULL,
+  `fast_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `passion` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `phone` int(15) NOT NULL,
+  `email` text NOT NULL,
+  `profile_pic` text NOT NULL,
+  `cover_pic` text NOT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT 1 COMMENT '0= inactive, 1=Active.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `account_seting`
+--
+
+INSERT INTO `account_seting` (`id`, `fast_name`, `last_name`, `passion`, `city`, `phone`, `email`, `profile_pic`, `cover_pic`, `status`) VALUES
+(1, 'MH', 'RONY', 'Web Development', 'Bogura', 1608445456, 'mh.rony.dhanvi@gmail.com', 'abc.jpg', '0', 1);
 
 -- --------------------------------------------------------
 
@@ -90,7 +116,7 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id`, `title`, `short_bio`, `email`, `phone_number`, `status`) VALUES
-(1, 'Contact Me', 'I am a Professional Web Developer.', 'mh.rony.dhanvi@gmail.com', 1608445456, 1),
+(1, 'Contact Me', 'sdfsdfsdfsdfsdf', 'sdfdfdfdf', 1608445456, 1),
 (2, 'Contact Me', 'I am a Professional Web Developer.', 'mh.rony.dhanvi@gmail.com', 1608445456, 1);
 
 -- --------------------------------------------------------
@@ -113,16 +139,18 @@ CREATE TABLE `education` (
 --
 
 INSERT INTO `education` (`id`, `education_name`, `short_bio`, `start_year`, `end_year`, `status`) VALUES
-(1, 'HSC', 'I Love Collage Life', 2018, 2020, 1),
-(2, 'Beck Stanley', 'Eaque alias amet in', 2015, 2010, 1),
-(3, 'Noel Weeks', 'Iusto quo iste in Na', 2014, 1999, 1),
-(4, 'Lois Bass', 'Modi omnis consectet', 1995, 1977, 1),
-(5, 'Kirestin Roberts', 'Tenetur dolores ut r', 2005, 1979, 1),
-(6, 'Wanda Camacho', 'Laborum Nam qui mag', 1996, 2011, 1),
-(7, 'Bethany Rosa', 'Eligendi molestiae o', 1994, 1997, 1),
-(8, 'Shad Mccullough', 'Proident corrupti ', 1999, 1971, 1),
-(9, 'Alexis Castro', 'Eveniet porro tempo', 1991, 2019, 1),
-(10, 'Kirsten Stephens', 'Eligendi dolor qui c', 2015, 2019, 1);
+(1, 'HSC', 'fgfgfgfgf', 2018, 2020, 0),
+(2, 'Beck Stanley', 'Eaque alias amet in', 2015, 2010, 0),
+(3, 'Noel Weeks', 'Iusto quo iste in Na', 2014, 1999, 0),
+(4, 'Lois Bass', 'Modi omnis consectet', 1995, 1977, 0),
+(5, 'Kirestin Roberts', 'Tenetur dolores ut r', 2005, 1979, 0),
+(6, 'Wanda Camacho', 'Laborum Nam qui mag', 1996, 2011, 0),
+(7, 'Bethany Rosa', 'Eligendi molestiae o', 1994, 1997, 0),
+(8, 'Shad Mccullough', 'Proident corrupti ', 1999, 1971, 0),
+(9, 'Alexis Castro', 'Eveniet porro tempo', 1991, 2019, 0),
+(10, 'Kirsten Stephens', 'Eligendi dolor qui c', 2015, 2019, 0),
+(11, 'Graham Hughes', 'Beatae ea quo commod', 1993, 1971, 0),
+(12, 'Kuame Foley', 'Culpa sunt officia ', 2009, 1998, 1);
 
 -- --------------------------------------------------------
 
@@ -144,18 +172,18 @@ CREATE TABLE `experience` (
 --
 
 INSERT INTO `experience` (`id`, `company_name`, `short_bio`, `start_jobe_date`, `end_jobe_date`, `status`) VALUES
-(1, 'Dhanvi', 'Nice ', 2022, '2022', 1),
-(2, 'Stanley and Davidson Associates', 'Nesciunt aspernatur', 8, '16-Aug-1996', 1),
-(3, 'Velasquez and Sandoval Co', 'Quis saepe maiores c', 31, '03-Oct-1980', 1),
-(4, 'Moran Mckenzie Co', 'Inventore eiusmod se', 27, '27-Nov-1985', 1),
-(5, 'Hensley and Leblanc LLC', 'Nobis atque aut dign', 8, '01-Nov-1995', 1),
-(6, 'Juarez and Rose Trading', 'Vitae alias enim ani', 10, '06-Oct-2004', 1),
-(7, 'Davenport and Pace Traders', 'Nostrud velit adipis', 27, '22-May-1972', 1),
-(8, 'Collins and Short Associates', 'Temporibus amet eni', 13, '10-Dec-1994', 1),
-(9, 'Mcdonald and Estes LLC', 'Enim cumque labore v', 15, '20-Sep-2010', 1),
-(10, 'Mccall Hays Traders', 'Et vitae aut est exe', 1983, '25-Aug-1991', 1),
-(11, 'Salinas Barber LLC', 'Eius maiores magnam ', 2016, '20-Jun-2019', 1),
-(12, 'Berry Stout LLC', 'Reprehenderit sit ', 1990, '18-Jul-2011', 1);
+(1, 'Dhanvi', 'dddddddd', 2022, '2022', 1),
+(2, 'Stanley and Davidson Associates', 'fghfghgfhgfh', 8, '16-Aug-1996', 0),
+(3, 'Velasquez and Sandoval Co', 'Quis saepe maiores c', 31, '03-Oct-1980', 0),
+(4, 'Moran Mckenzie Co', 'Inventore eiusmod se', 27, '27-Nov-1985', 0),
+(5, 'Hensley and Leblanc LLC', 'Nobis atque aut dign', 8, '01-Nov-1995', 0),
+(6, 'Juarez and Rose Trading', 'Vitae alias enim ani', 10, '06-Oct-2004', 0),
+(7, 'Davenport and Pace Traders', 'Nostrud velit adipis', 27, '22-May-1972', 0),
+(8, 'Collins and Short Associates', 'Temporibus amet eni', 13, '10-Dec-1994', 0),
+(9, 'Mcdonald and Estes LLC', 'Enim cumque labore v', 15, '20-Sep-2010', 0),
+(10, 'Mccall Hays Traders', 'Et vitae aut est exe', 1983, '25-Aug-1991', 0),
+(11, 'Salinas Barber LLC', 'Eius maiores magnam ', 2016, '20-Jun-2019', 0),
+(12, 'Berry Stout LLC', 'Reprehenderit sit ', 1990, '18-Jul-2011', 0);
 
 -- --------------------------------------------------------
 
@@ -177,7 +205,7 @@ CREATE TABLE `home_edit_section` (
 --
 
 INSERT INTO `home_edit_section` (`id`, `your_name`, `description`, `link`, `image`, `status`) VALUES
-(1, 'MH RONY', 'eloper.', 'facebook.com', 'abc.jpg', 1);
+(1, 'MH RONY', 'sdfsdfdsfdsfsdfsdfsdf', 'facebook.com', '', 1);
 
 -- --------------------------------------------------------
 
@@ -187,14 +215,14 @@ INSERT INTO `home_edit_section` (`id`, `your_name`, `description`, `link`, `imag
 
 CREATE TABLE `portfolio` (
   `id` int(11) NOT NULL,
-  `project_name` varchar(100) NOT NULL,
+  `project_name` varchar(50) NOT NULL,
   `project_bio` text NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `clint_name` varchar(100) NOT NULL,
-  `project_language` varchar(255) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `clint_name` varchar(50) NOT NULL,
+  `project_language` varchar(50) NOT NULL,
   `project_preview_link` text NOT NULL,
   `image` text NOT NULL,
-  `status` tinyint(2) NOT NULL DEFAULT 1 COMMENT '0= inactive, 1= active'
+  `status` tinyint(2) NOT NULL DEFAULT 1 COMMENT '0= inactive, 1= Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -202,20 +230,7 @@ CREATE TABLE `portfolio` (
 --
 
 INSERT INTO `portfolio` (`id`, `project_name`, `project_bio`, `category`, `clint_name`, `project_language`, `project_preview_link`, `image`, `status`) VALUES
-(1, 'Admin Panel', 'Web Developer project file', 'Web Development', 'Korim', 'HTML, CSS, Javascript', 'preview.com', 'abc.jpg', 1),
-(2, 'Ciara Stone', 'Iure voluptas id lor', 'Duis qui fugiat temp', 'Shelly Robles', 'Consequatur quia ni', 'Omnis voluptatibus n', '', 1),
-(3, 'Ciaran Raymond', 'Ea cupiditate atque ', 'Commodi exercitation', 'Meredith Leach', 'Sint cillum Nam exer', 'Officiis quis enim i', '', 1),
-(4, 'Cole Golden', 'Consequatur qui sae', 'Ea est exercitatione', 'Venus Stafford', 'Ad est atque ratione', 'Quo quis eaque dolor', '', 1),
-(5, 'Luke Gregory', 'Do id quas velit ex', 'Voluptatem Placeat', 'Shoshana Slater', 'Quis ut consectetur', 'Sed quia vel tempore', '', 1),
-(6, 'Montana Knox', 'Alias non ut recusan', 'Incidunt libero ill', 'Aileen Kline', 'Voluptas ea earum te', 'Aut veniam officiis', '', 1),
-(7, 'Paki Mckinney', 'Cupidatat ipsa exer', 'Iste dolor aut delec', 'Charlotte Boyd', 'Dolore autem volupta', 'Recusandae Beatae s', '', 1),
-(8, 'Octavius Dale', 'Sequi culpa impedit', 'Corrupti nesciunt ', 'Buckminster Barnes', 'Eius commodi consequ', 'Sunt blanditiis natu', '', 1),
-(9, 'Anastasia Manning', 'Ad fuga Et ut sit ', 'Harum consequuntur e', 'Colleen Gray', 'Dolore odit sunt aut', 'Neque laborum neque ', '', 1),
-(10, 'Murphy Luna', 'Est quo blanditiis n', 'Ullamco ad alias ips', 'Regan Morris', 'Excepturi consequat', 'Veniam excepteur re', '', 1),
-(11, 'Rajah Castillo', 'Dolor ratione archit', 'Eum facilis ratione ', 'Iris Nelson', 'Molestias nisi tenet', 'Beatae exercitation ', '', 1),
-(12, 'Chava Cameron', 'Dolores quasi assume', 'Lorem enim sequi aut', 'Iris Morales', 'Nesciunt sit et lab', 'Ut rerum iusto nostr', '', 1),
-(13, 'Kelly Leach', 'Quo corrupti elit ', 'Et ea consequuntur e', 'Clinton Grant', 'Proident doloribus ', 'Repudiandae corrupti', '', 1),
-(14, 'Gareth Wise', 'Velit possimus reru', 'Laudantium earum ve', 'Drake England', 'Ea aute et iste in m', 'Nostrud iusto minim ', '', 1);
+(1, 'Hilda Gill', 'Illo dolor est odio ', 'Ipsum ipsam est se', 'Wayne Benjamin', 'Error minima ab occa', 'Qui dolor animi iru', '1656671505.png', 1);
 
 -- --------------------------------------------------------
 
@@ -236,16 +251,18 @@ CREATE TABLE `skill` (
 --
 
 INSERT INTO `skill` (`id`, `skill_name`, `percentage`, `experience`, `status`) VALUES
-(1, 'PHP', 76, '1', 1),
+(1, 'PHP', 765353, '1', 0),
 (2, 'HTML', 86, '3', 1),
-(3, 'Martina Ferguson', 0, 'Non veniam qui maio', 1),
-(4, 'Roth Eaton', 0, 'Commodi dolores veli', 1),
-(5, 'Lana Clemons', 0, 'Dolor alias aut est', 1),
-(6, 'Blaine Alvarez', 0, 'Vero eveniet a corp', 1),
-(7, 'Imani Nielsen', 0, 'In pariatur Dolorem', 1),
-(8, 'Cathleen Mack', 120, 'Ea exercitation illo', 1),
-(9, 'Vielka Henderson', 110, 'Deleniti voluptatum ', 1),
-(10, 'Jenette Randolph', 500, 'Ipsum rerum ex ea e', 1);
+(3, 'Martina Ferguson', 0, 'Non veniam qui maio', 0),
+(4, 'Roth Eaton', 0, 'Commodi dolores veli', 0),
+(5, 'Lana Clemons', 0, 'Dolor alias aut est', 0),
+(6, 'Blaine Alvarez', 0, 'Vero eveniet a corp', 0),
+(7, 'Imani Nielsen', 0, 'In pariatur Dolorem', 0),
+(8, 'Cathleen Mack', 120, 'Ea exercitation illo', 0),
+(9, 'Vielka Henderson', 110, 'Deleniti voluptatum ', 0),
+(10, 'Jenette Randolph', 500, 'Ipsum rerum ex ea e', 0),
+(11, 'Ignatius Duncan', 0, 'Cupidatat unde corru', 0),
+(12, 'Jared Contreras', 0, 'Rerum aute in ad dol', 0);
 
 -- --------------------------------------------------------
 
@@ -265,21 +282,21 @@ CREATE TABLE `social_media` (
 --
 
 INSERT INTO `social_media` (`id`, `social_media_name`, `social_media_link`, `status`) VALUES
-(1, 'facebook', 'facebook.com', 1),
-(2, 'facebook', 'facebook.com', 1),
-(3, 'Acton Rosales', 'Consequatur Quia su', 1),
-(4, 'Acton Rosales', 'Consequatur Quia su', 1),
-(5, 'Acton Rosales', 'Consequatur Quia su', 1),
-(6, 'Acton Rosales', 'Consequatur Quia su', 1),
-(7, 'Acton Rosales', 'Consequatur Quia su', 1),
-(8, 'Hanna Marsh', 'Distinctio Nesciunt', 1),
-(9, 'Caldwell Campos', 'Eiusmod reprehenderi', 1),
-(10, 'Sophia Schwartz', 'Beatae alias ex reru', 1),
-(11, 'Acton Rosales', 'Consequatur Quia su', 1),
-(12, 'Acton Rosales', 'Consequatur Quia su', 1),
-(13, 'Mufutau Mendoza', 'Dolor nostrud dolore', 1),
-(14, 'Avram Monroe', 'Ex non est ea illo ', 1),
-(15, 'Colt Lynch', 'Sit unde at cupidita', 1);
+(1, 'facebooksdfsdfsdfsdfsdfsdf', 'facebook.com', 1),
+(2, 'facebook', 'facebook.com', 0),
+(3, 'Acton Rosales', 'Consequatur Quia su', 0),
+(4, 'Acton Rosales', 'Consequatur Quia su', 0),
+(5, 'Acton Rosales', 'Consequatur Quia su', 0),
+(6, 'Acton Rosales', 'Consequatur Quia su', 0),
+(7, 'Acton Rosales', 'Consequatur Quia su', 0),
+(8, 'Hanna Marsh', 'Distinctio Nesciunt', 0),
+(9, 'Caldwell Campos', 'Eiusmod reprehenderi', 0),
+(10, 'Sophia Schwartz', 'Beatae alias ex reru', 0),
+(11, 'Acton Rosales', 'Consequatur Quia su', 0),
+(12, 'Acton Rosales', 'Consequatur Quia su', 0),
+(13, 'Mufutau Mendoza', 'Dolor nostrud dolore', 0),
+(14, 'Avram Monroe', 'Ex non est ea illo ', 0),
+(15, 'Colt Lynch', 'Sit unde at cupidita', 0);
 
 --
 -- Indexes for dumped tables
@@ -295,6 +312,12 @@ ALTER TABLE `about_info`
 -- Indexes for table `about_work`
 --
 ALTER TABLE `about_work`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `account_seting`
+--
+ALTER TABLE `account_seting`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -356,6 +379,12 @@ ALTER TABLE `about_work`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `account_seting`
+--
+ALTER TABLE `account_seting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
@@ -365,7 +394,7 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `education`
 --
 ALTER TABLE `education`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `experience`
@@ -383,13 +412,13 @@ ALTER TABLE `home_edit_section`
 -- AUTO_INCREMENT for table `portfolio`
 --
 ALTER TABLE `portfolio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `skill`
 --
 ALTER TABLE `skill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `social_media`
